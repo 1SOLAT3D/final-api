c9841c9e753db57cb4d10339b5704e7c1a86c543
Gianfranco Martínez Cisneros <francodortmund84@gmail.com>
2023-12-05 08:42:15 -0600

    Updated

--- ProyectoFinalAPIREST/config.js
+++
@@ -1,9 +0,0 @@
-import {config} from 'dotenv'
-
-config();
-
-export const MYSQLHOST = process.env.MYSQLHOST || "localhost";
-export const MYSQLUSER = process.env.MYSQLUSER || "root";
-export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || "";
-export const MYSQLDATABASE = process.env.MYSQLDATABASE || "lec2023";
-export const MYSQLPORT = process.env.MYSQLPORT || 3306;
